37213333cd10c92bf88a718427dc76395943a933
SanuKumar <sanukumar1138@gmail.com>
2022-01-24 11:16:30 +0530

    removed info

--- containers/Home.js
+++ containers/Home.js
@@ -36,7 +36,6 @@ const Home = ({ navigation }) => {
         source={{
           uri: "https://cdn.dribbble.com/users/9707868/screenshots/16799890/hfff_1_3_4x.png",
           // uri: "https://picsum.photos/id/237/200/300",
-          // uri: "https://instagram.fixr1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p750x750/271669492_680934443290792_3017890099147487546_n.jpg?_nc_ht=instagram.fixr1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=CKlbGadEh1YAX9rzToJ&edm=ALQROFkBAAAA&ccb=7-4&ig_cache_key=Mjc1MDEwODgxMTk3ODkwODg4MQ%3D%3D.2-ccb7-4&oh=00_AT9m2Y2uKCsZeK_halN6b-KEaX4YNOoWazE_XNHA6IjkFQ&oe=61F4AD0D&_nc_sid=30a2ef",
         }}
       />
       <View style={styles.button_main}>
